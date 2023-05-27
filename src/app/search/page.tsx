026620ef68cd2251e6c5e99640c40751aaf44d2a
DESKTOP-9VU9I7S\Rai <ajipunk008@gmail.com>
2023-05-28 01:22:29 +0700

    fix ticket responsive

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,7 +3,7 @@ import SearchResult from "./search-result";
 export default function Search() {
     return (
         <section className="py-16">
-            <div className="container px-20 mx-auto">
+            <div className="container lg:px-20 px-5 mx-auto">
                 <SearchResult />
             </div>
         </section>
